7f968daa45a5135bf3af43df51ec6939b916b1ff
itzVort3x1 <saikaustubh10@gmail.com>
2022-04-09 11:30:39 +0530

    finished code

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 
 import scapy.all as scapy
 
-
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_bradcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_bradcast, timeout=1, verbose=False)[0]
+
+    print("IP\t\t\t\tMAC Address")
+    print("--------------------------------------------------------------")
+    for element in answered_list:
+        print(element[1].psrc + "\t\t\t" + element[1].hwsrc)
+    # scapy.ls is used to list out the options of a scapy attribute
+    # scapy.ls(scapy.Ether())
 
 
 scan("192.168.174.2/24")
